8c23a8ab338f04a135e9924013cd07098d5d6770
24045990CaseyRP <24045990@myrp.edu.sg>
2026-01-07 10:53:45 +0800

    updated code

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const dbConfig = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    waitForConnection: true,
+    waitForConnections: true,
     connectionLimit: 100,
     queueLimit: 0,
 };
@@ -22,19 +22,19 @@ app.use(express.json());
 
 //start the server
 app.listen(port, () =>{
-    console.log(`Server running on port`, port);
+    console.log('Server running on port', port);
 });
 
 //Example Route: Get all cards
 app.get('/allcards', async (req, res) => {
     try{
-        let connection = await mysql.createConnection(dbconfig);
+        let connection = await mysql.createConnection(dbConfig);
         const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
         res.json(rows);
     }
     catch(err){
         console.log(err);
-        res.status(500).json({message: ' Server error for allcards'});
+        res.status(500).json({message: 'Server error for allcards'});
     }
 });
 
