3444cec6153e95fe971d4f99a1dd8db660f5b2ff
24045990CaseyRP <24045990@myrp.edu.sg>
2026-01-22 19:13:15 +0800

    changed servers

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://card-app-smoky.vercel.app",
   "https://onlinecardappwebservice-tmj4.onrender.com",
-  "https://card-app-starter-supateam-onpjeb2h3-caseys-projects-b1ab35b8.vercel.app"
+  "https://card-app-starter-supateam-igtlsjdyw-caseys-projects-b1ab35b8.vercel.app",
+  
 ];
 
 app.use(
